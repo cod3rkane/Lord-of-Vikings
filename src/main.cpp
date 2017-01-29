ca1d523d9e1a29218323bf438d3d2d28a5d849c5
cod3rkane <jcesar.developer@gmail.com>
2017-01-29 10:59:52 -0200

    my fist triangle

--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,22 @@ int main() {
     glViewport(0, 0, 1280, 720);
     glMatrixMode(GL_MODELVIEW);
 
+    // OpenGL Code
+
+    GLuint vertexArrayID;
+    glGenVertexArrays(1, &vertexArrayID);
+    glBindVertexArray(vertexArrayID);
+
+    static const GLfloat vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f
+    };
+
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
     Shader myShader;
     myShader.loadFiles("shader.vert", "shader.frag");
 
@@ -40,6 +56,15 @@ int main() {
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(0.0, 0.0, 0.0, 1.0f);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+        myShader.useProgram();
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
 
         glfwSwapBuffers(window);
     }
