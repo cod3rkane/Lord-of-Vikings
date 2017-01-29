a3c2b62aa6a31cb0aee30d0b0bafadac9ac662ec
cod3rkane <jcesar.developer@gmail.com>
2017-01-29 10:32:05 -0200

    Complete Shader Struct and added new configs

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,12 @@ int main() {
         return -1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
     window = glfwCreateWindow(1280, 720, "Lord of Vikings", NULL, NULL);
 
     if (!window) {
@@ -19,15 +25,26 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    glMatrixMode(GL_PROJECTION);
+    glViewport(0, 0, 1280, 720);
+    glMatrixMode(GL_MODELVIEW);
+
     Shader myShader;
     myShader.loadFiles("shader.vert", "shader.frag");
 
     while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
         glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.0, 0.0, 0.0, 1.0f);
+
+        glfwSwapBuffers(window);
     }
 
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
--- src/util/Shader.cpp
+++ src/util/Shader.cpp
@@ -1,8 +1,6 @@
 #include "Shader.h"
 
 void Shader::loadFiles(const GLchar *vertexPath, const GLchar *fragmentPath) {
-    std::cout << "Hello World!" << std::endl;
-
     std::ifstream shaderStream(vertexPath, std::ios::binary), fragmentStream(fragmentPath, std::ios::binary);
     std::stringstream vShaderString, fShaderString;
 
@@ -12,12 +10,59 @@ void Shader::loadFiles(const GLchar *vertexPath, const GLchar *fragmentPath) {
     shaderStream.close();
     fragmentStream.close();
 
-    std::string vertexCode, fragmentCode;
+    std::string vertexCode = vShaderString.str();
+    std::string fragmentCode = fShaderString.str();
 
-    vertexCode = vShaderString.str();
-    fragmentCode = fShaderString.str();
+    const GLchar *vCode = vertexCode.c_str();
+    const GLchar *fCode = fragmentCode.c_str();
 
-    std::cout << vertexCode << std::endl;
+    // Compile Shaders
+    GLuint vertex, fragment;
+    GLint success;
+    GLchar infolog[512];
+
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vCode, NULL);
+    glCompileShader(vertex);
+
+    // check erros
+    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex, 512, NULL, infolog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << std::endl << infolog << std::endl;
+    }
+
+    // Compile Fragment
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fCode, NULL);
+    glCompileShader(fragment);
+
+    // check errors
+    glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment, 512, NULL, infolog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << std::endl << infolog << std::endl;
+    }
+
+    // Shader Program
+    this->programID = glCreateProgram();
+    if (this->programID == 0) {
+        std::cout << "ERROR::SHADER::PROGRAMID::ON_CREATE_PROGRAMID" << std::endl;
+    }
+
+    glAttachShader(this->programID, vertex);
+    glAttachShader(this->programID, fragment);
+    glLinkProgram(this->programID);
+
+    // check linking errors
+    glGetProgramiv(this->programID, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(this->programID, 512, NULL, infolog);
+        std::cout << "ERROR::SHADER::PROGRAMID::LINKING_FAILED" << std::endl << infolog << std::endl;
+    }
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
 }
 
 void Shader::useProgram() {
